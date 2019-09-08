dad0d71940a14d58844580e682801ee973c79a68
Pele <bennett.johanson@gmail.com>
2019-09-08 11:00:00 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+//import statements
+const express = require('express');
+const mongoose=require('mongoose');
+
+//create instance of express
+const app = express();
+
+//Db config
+const db=require('./config/keys').mongoURI;
+
+//connect to mongodb
+mongoose.connect(db)
+.then(()=> console.log('MongoDb connected'))
+.catch(err => console.log(err));
+
+//create first route
+//allow user to get data back
+app.get('/', (req,res) => res.send('hello'));
+
+const port = 7000;
+
+//tell express to listen on this port
+app.listen(port, ()=>console.log(`Server running on port ${port}`));
