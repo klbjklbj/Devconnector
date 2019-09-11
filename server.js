165f80e8f9d378fb2786e66af8c8f35db70cd019
Pele <bennett.johanson@gmail.com>
2019-09-10 19:02:03 -0700

    2nd commit

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 //import statements
-const express = require('express');
-const mongoose=require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 //create instance of express
 const app = express();
 
 //Db config
-const db=require('./config/keys').mongoURI;
+//connection string in keys file
+const db = require("./config/keys").mongoURI;
 
 //connect to mongodb
-mongoose.connect(db)
-.then(()=> console.log('MongoDb connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(db)
+  //prmoise statement
+  .then(() => console.log("MongoDb connected"))
+  .catch(err => console.log(err));
 
 //create first route
 //allow user to get data back
-app.get('/', (req,res) => res.send('hello'));
+app.get("/", (req, res) => res.send("hello"));
 
 const port = 7000;
 
 //tell express to listen on this port
-app.listen(port, ()=>console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
