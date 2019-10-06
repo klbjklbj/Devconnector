03a782882648ee2b61bd04a6297accd693671e08
Kristen Johanson <43449427+klbjklbj@users.noreply.github.com>
2019-10-06 01:54:56 -0700

    work on components and state

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer class="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Dev Connector
+    </footer>
+  );
 }
